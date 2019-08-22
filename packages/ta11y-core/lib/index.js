809574cd1e44c8cbc95f2ede0c449f67b3994b33
Travis Fischer <fisch0920@gmail.com>
2019-08-21 21:38:53 -0500

    feat: add audit support for remote extraction

--- packages/ta11y-core/lib/index.js
+++ packages/ta11y-core/lib/index.js
@@ -2,6 +2,8 @@
 
 const { extract } = require('@ta11y/extract')
 const got = require('got')
+const isHtml = require('is-html')
+const isUrl = require('is-url-superb')
 
 exports.Ta11y = class Ta11y {
   constructor(opts = {}) {
@@ -20,9 +22,14 @@ exports.Ta11y = class Ta11y {
   }
 
   async audit(urlOrHtml, opts) {
-    const extractResults = await extract(urlOrHtml, opts)
+    if (!opts || opts.remote) {
+      delete opts.remote
+      return this._remoteAudit(urlOrHtml, opts)
+    } else {
+      const extractResults = await extract(urlOrHtml, opts)
 
-    return this.auditExtractResults(extractResults)
+      return this.auditExtractResults(extractResults)
+    }
   }
 
   async auditExtractResults(extractResults) {
@@ -30,6 +37,33 @@ exports.Ta11y = class Ta11y {
     const auditResults = await got
       .post(apiAuditUrl, {
         json: extractResults,
+        headers: this._headers
+      })
+      .json()
+
+    return auditResults
+  }
+
+  async _remoteAudit(urlOrHtml, opts) {
+    let url
+    let html
+
+    if (isUrl(urlOrHtml)) {
+      url = urlOrHtml
+    } else if (isHtml(urlOrHtml)) {
+      html = urlOrHtml
+    } else {
+      throw new Error('audit expects either a URL or HTML input')
+    }
+
+    const apiAuditUrl = `${this.apiBaseUrl}/audit`
+    const auditResults = await got
+      .post(apiAuditUrl, {
+        json: {
+          ...opts,
+          url,
+          html
+        },
         headers: this._headers
       })
       .json()
