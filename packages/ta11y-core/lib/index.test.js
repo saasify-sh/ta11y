208c3facef481b7524d9f3a81bd6e9c59c99b9ca
Travis Fischer <fisch0920@gmail.com>
2020-01-08 17:07:01 -0500

    🏁

--- packages/ta11y-core/lib/index.test.js
+++ packages/ta11y-core/lib/index.test.js
@@ -9,7 +9,12 @@ const launchOptions = { headless: true }
 
 test('wikipedia url local extract', async (t) => {
   const scope = nock('https://ssfy.sh')
-    .post('/dev/ta11y/auditExtractResults', (body) => true)
+    .post('/dev/ta11y/auditExtractResults', (body) => {
+      t.is(body.summary.visited, 1)
+      t.is(body.summary.success, 1)
+      t.is(body.summary.error, 0)
+      return true
+    })
     .reply(200, {
       foo: 'bar'
     })
@@ -25,4 +30,21 @@ test('wikipedia url local extract', async (t) => {
   t.true(scope.isDone())
 
   await browser.close()
+})
+
+test('wikipedia url remote extract', async (t) => {
+  const scope = nock('https://ssfy.sh')
+    .post('/dev/ta11y/audit', (body) => {
+      t.deepEqual(body, { url: 'https://en.wikipedia.org' })
+      return true
+    })
+    .reply(200, {
+      foo: 'bar'
+    })
+
+  const ta11y = new Ta11y()
+  const result = await ta11y.audit('https://en.wikipedia.org')
+
+  t.deepEqual(result, { foo: 'bar' })
+  t.true(scope.isDone())
 })
