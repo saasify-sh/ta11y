2243f3d57c9cb3d9faa35a0d42d18f8173abb9bc
Travis Fischer <fisch0920@gmail.com>
2020-01-13 20:15:53 -0500

    🍌

---
+++ examples/on-prem/index.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const puppeteer = require('puppeteer')
+const { Ta11y } = require('@ta11y/core')
+const { auditExtractResults } = require('@ta11y/on-prem')
+
+const main = async () => {
+  // Create a browser that we'll initialize with cookies and then use to run the ta11y audit
+  const browser = await puppeteer.launch({ headless: false, slowMo: 10 })
+  const ta11y = new Ta11y()
+
+  const extractResults = await ta11y.audit('https://instagram.com', {
+    browser,
+    crawl: true,
+    maxDepth: 1,
+    maxVisit: 4,
+    extractOnly: true
+  })
+  browser.close()
+
+  const results = await auditExtractResults(extractResults)
+  console.log(JSON.stringify(results, null, 2))
+}
+
+main()
+  .then(() => {
+    process.exit(0)
+  })
+  .catch((err) => {
+    console.error(err)
+    process.exit(1)
+  })
---
+++ examples/on-prem/readme.md
@@ -0,0 +1,17 @@
+<p align="center">
+  <a href="https://ta11y.saasify.sh" title="ta11y">
+    <img src="https://raw.githubusercontent.com/saasify-sh/ta11y/master/media/logo.svg?sanitize=true" alt="ta11y Logo" width="256" />
+  </a>
+</p>
+
+# ta11y-example-on-prem
+
+> Example auditing a site on-prem.
+
+[![NPM](https://img.shields.io/npm/v/@ta11y/ta11y.svg)](https://www.npmjs.com/package/@ta11y/ta11y) [![Build Status](https://travis-ci.com/saasify-sh/ta11y.svg?branch=master)](https://travis-ci.com/saasify-sh/ta11y) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+**Note**: this requires an enterprise plan to access the private `@ta11y/on-prem` package.
+
+## License
+
+MIT © [Saasify](https://saasify.sh)
